9ec815c1ca2c26a436b4d18a46341d03c1248a22
Kostas Vlachos <kvl_93@outlook.com>
2019-07-17 23:28:50 +0300

    Added typeparam tags in the documentation comments of classes in the BurstChat.Shared.Monads namespace

--- BurstChat.Shared/Monads/Failure.cs
+++ BurstChat.Shared/Monads/Failure.cs
@@ -3,10 +3,16 @@ using System.Threading.Tasks;
 
 namespace BurstChat.Shared.Monads
 {
+    /// <summary>
+    ///   This class is a monad implementation of Either that can be used to represent a failed execution
+    ///   of a method.
+    /// </summary>
+    /// <typeparam name="TSuccess">The type encapsulated by the success instance of the monad</typeparam>
+    /// <typeparam name="TFailure">The type encapsulated by the failure instance of the monad</typeparam>
     public class Failure<TSuccess, TFailure> : Either<TSuccess, TFailure>
     {
         /// <summary>
-        /// The value contained within the monad.
+        ///   The value contained within the monad.
         /// </summary>
         public TFailure Value
         {
@@ -14,7 +20,7 @@ namespace BurstChat.Shared.Monads
         }
 
         /// <summary>
-        /// Executes any necessary start up code for the monad.
+        ///   Executes any necessary start up code for the monad.
         /// </summary>
         public Failure(TFailure value)
         {
@@ -22,9 +28,10 @@ namespace BurstChat.Shared.Monads
         }
 
         /// <summary>
-        /// This method in a success scenario will invoke the callback provided and return its
-        /// results, and will not in a failure scenario.
+        ///   This method in a success scenario will invoke the callback provided and return its
+        ///   results, and will not in a failure scenario.
         /// </summary>
+        /// <typeparam name="TOut">The type encapsulated by the resulting Either monad</typeparam>
         /// <param name="callback">The callback to be executed</param>
         /// <returns>An either monad</returns>
         public override Either<TOut, TFailure> Bind<TOut>(Func<TSuccess, Either<TOut, TFailure>> callback)
@@ -33,9 +40,10 @@ namespace BurstChat.Shared.Monads
         }
 
         /// <summary>
-        /// This method in a success scenario will invoke the callback asynchronously and return its
-        /// results. In a failure scenario it will not invoke the callback at all.
+        ///   This method in a success scenario will invoke the callback asynchronously and return its
+        ///   results. In a failure scenario it will not invoke the callback at all.
         /// </summary>
+        /// <typeparam name="TOut">The type encapsulated by the resulting Either monad</typeparam>
         /// <param name="callbackTask">The callback task to be executed</param>
         /// <return>A task of an either monad</returns>
         public override Task<Either<TOut, TFailure>> BindAsync<TOut>(Func<TSuccess, Task<Either<TOut, TFailure>>> callback)
@@ -44,10 +52,11 @@ namespace BurstChat.Shared.Monads
         }
 
         /// <summary>
-        /// This method will invoke the provided callback and will wrap the resulting instance
-        /// to an either monad and return it. This behavious is in a success scenario, the callback
-        /// will not execute in a failure scenario.
+        ///   This method will invoke the provided callback and will wrap the resulting instance
+        ///   to an either monad and return it. This behavious is in a success scenario, the callback
+        ///   will not execute in a failure scenario.
         /// </summary>
+        /// <typeparam name="TOut">The type encapsulated by the resulting Either monad</typeparam>
         /// <param name="callback">The callback to be executed</param>
         /// <returns>An either monad</returns>
         public override Either<TOut, TFailure> Attach<TOut>(Func<TSuccess, TOut> callback)
@@ -56,10 +65,11 @@ namespace BurstChat.Shared.Monads
         }
 
         /// <summary>
-        /// This method will invoke the provided callback asynchronously and will wrap the resulting instance
-        /// to an either monad and return it. This behavious is in a success scenario, the callback
-        /// will not execute in a failure scenario.
+        ///   This method will invoke the provided callback asynchronously and will wrap the resulting instance
+        ///   to an either monad and return it. This behavious is in a success scenario, the callback
+        ///   will not execute in a failure scenario.
         /// </summary>
+        /// <typeparam name="TOut">The type encapsulated by the resulting Either monad</typeparam>
         /// <param name="callback">The callback to be executed</param>
         /// <returns>A task of an either monad</returns>
         public override Task<Either<TOut, TFailure>> AttachAsync<TOut>(Func<TSuccess, Task<TOut>> callback)
@@ -68,8 +78,8 @@ namespace BurstChat.Shared.Monads
         }
 
         /// <summary>
-        /// This method will execute the provided callback and simply return it self at the end.
-        /// This behaviour is the same in a success and in a failure scenario.
+        ///   This method will execute the provided callback and simply return it self at the end.
+        ///   This behaviour is the same in a success and in a failure scenario.
         /// </summary>
         /// <param name="callback">The callback to be executed</param>
         /// <returns>An either monad</returns>
@@ -79,8 +89,8 @@ namespace BurstChat.Shared.Monads
         }
 
         /// <summary>
-        /// This method will execute the provided callback asynchronously and simply return it self at the end.
-        /// This behaviour is the same in a success and in a failure scenario.
+        ///   This method will execute the provided callback asynchronously and simply return it self at the end.
+        ///   This behaviour is the same in a success and in a failure scenario.
         /// </summary>
         /// <param name="callback">The callback to be executed</param>
         /// <returns>An either monad</returns>
