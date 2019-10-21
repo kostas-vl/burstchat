42dc703305949ca3b8cdf39a54590cd9b76d88d3
Kostas Vlachos <kvl_93@outlook.com>
2019-10-21 20:36:46 +0300

    Minor ui changes.

--- BurstChat.Client/src/app/modules/chat/components/chat-info/chat-info.component.ts
+++ BurstChat.Client/src/app/modules/chat/components/chat-info/chat-info.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { ChatConnectionOptions } from 'src/app/models/chat/chat-connection-options';
+import { ChannelConnectionOptions } from 'src/app/models/chat/channel-connection-options';
+import { PrivateGroupConnectionOptions } from 'src/app/models/chat/private-group-connection-options';
+import { DirectMessagingConnectionOptions } from 'src/app/models/chat/direct-messaging-connection-options';
 
 /**
  * This class represents an angular component that displays on screen the top bar of the application
@@ -10,14 +13,26 @@ import { ChatConnectionOptions } from 'src/app/models/chat/chat-connection-optio
  * @implements {OnInit}
  */
 @Component({
-  selector: 'app-chat-info',
-  templateUrl: './chat-info.component.html',
-  styleUrls: ['./chat-info.component.scss']
+    selector: 'app-chat-info',
+    templateUrl: './chat-info.component.html',
+    styleUrls: ['./chat-info.component.scss']
 })
 export class ChatInfoComponent implements OnInit {
 
     @Input()
     public options?: ChatConnectionOptions;
+
+    public get icon() {
+        if (this.options instanceof ChannelConnectionOptions) {
+            return 'lni-bubble';
+        } else if (this.options instanceof PrivateGroupConnectionOptions) {
+            return 'lni-network';
+        } else if (this.options instanceof DirectMessagingConnectionOptions) {
+            return 'lni-users';
+        } else {
+            return '';
+        }
+    }
 
     /**
      * Creates a new instance of ChatInfoComponent.
