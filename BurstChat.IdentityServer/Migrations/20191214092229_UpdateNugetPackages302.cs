06175efcb1d2f36716ee6123a5aa24260e2edef5
Kostas Vlachos <kvl_93@outlook.com>
2019-12-14 11:26:23 +0200

    Updated the Identity project to dotnet core 3.1 Update all nuget packages. Added new migrations

---
+++ BurstChat.IdentityServer/Migrations/20191214092229_UpdateNugetPackages302.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BurstChat.IdentityServer.Migrations
+{
+    public partial class UpdateNugetPackages302 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- BurstChat.IdentityServer/Migrations/ConfigurationDb/20191214092107_UpdateNugetPackages302.Designer.cs
+++ BurstChat.IdentityServer/Migrations/ConfigurationDb/20191214092107_UpdateNugetPackages302.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using IdentityServer4.EntityFramework.DbContexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace BurstChat.IdentityServer.Migrations.ConfigurationDb
 {
     [DbContext(typeof(ConfigurationDbContext))]
-    partial class ConfigurationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20191214092107_UpdateNugetPackages302")]
+    partial class UpdateNugetPackages302
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
