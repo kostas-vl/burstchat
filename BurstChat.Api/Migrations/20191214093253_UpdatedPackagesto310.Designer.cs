83d13cc94cfa3d3cb3fffb26b63fa6f034aac1d6
Kostas Vlachos <kvl_93@outlook.com>
2019-12-14 11:33:52 +0200

    Updated project to dotnet core 3.1 Updated all nuget packages Added migrations

--- BurstChat.Api/Migrations/20191214093253_UpdatedPackagesto310.Designer.cs
+++ BurstChat.Api/Migrations/20191214093253_UpdatedPackagesto310.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using BurstChat.Shared.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace BurstChat.Api.Migrations
 {
     [DbContext(typeof(BurstChatContext))]
-    partial class BurstChatContextModelSnapshot : ModelSnapshot
+    [Migration("20191214093253_UpdatedPackagesto310")]
+    partial class UpdatedPackagesto310
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
