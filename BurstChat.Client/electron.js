a5bd6931d04f16fce10665cafe1e10e7c1b68fb7
Kostas Vlachos <kvl_93@outlook.com>
2019-12-14 15:02:29 +0200

    Minor addition to the electron script

--- BurstChat.Client/electron.js
+++ BurstChat.Client/electron.js
@@ -8,6 +8,7 @@ function createWindow() {
     mainWindow = new BrowserWindow({
         width: 800,
         height: 600,
+        show: false,
         webPreferences: {
             nodeIntegration: true
         }
@@ -24,21 +25,24 @@ function createWindow() {
     // Open the DevTools.
     mainWindow.webContents.openDevTools();
 
+    mainWindow.on('ready-to-show', function() {
+        mainWindow.show()
+    });
 
-    mainWindow.on('closed', function () {
+    mainWindow.on('closed', () => {
         mainWindow = null;
-    })
+    });
 }
 
 app.on('ready', createWindow)
 
-app.on('window-all-closed', function () {
+app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
         app.quit();
     }
 })
 
-app.on('activate', function () {
+app.on('activate', () => {
     if (mainWindow === null) {
         createWindow();
     }
