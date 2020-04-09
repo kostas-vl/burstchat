c6bdac999fa2addef825bbee1bbc374975c878f7
Kostas Vlachos <kvl_93@outlook.com>
2020-04-09 22:54:24 +0300

    New migration code for the AlphaInvitation model

--- BurstChat.Api/Migrations/20200409195309_AddedAlphaInvitation.Designer.cs
+++ BurstChat.Api/Migrations/20200409195309_AddedAlphaInvitation.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using BurstChat.Shared.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace BurstChat.Api.Migrations
 {
     [DbContext(typeof(BurstChatContext))]
-    partial class BurstChatContextModelSnapshot : ModelSnapshot
+    [Migration("20200409195309_AddedAlphaInvitation")]
+    partial class AddedAlphaInvitation
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
