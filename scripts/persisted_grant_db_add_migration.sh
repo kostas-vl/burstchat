8906620f05cc4fdaa10085728ede217ef956c708
Kostas Vlachos <kvl_93@outlook.com>
2020-07-12 11:48:23 +0300

    Added the new migration scripts.

---
+++ scripts/persisted_grant_db_add_migration.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+dotnet ef migrations add $1 --project ../BurstChat.Infrastructure/ --startup-project . --output-dir ../BurstChat.Infrastructure/Persistence/Migrations/PersistedGrantDb -c PersistedGrantDbContext
