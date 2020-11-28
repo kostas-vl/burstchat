3d08984c3e8983572fe788b627828be33acc6e6d
Kostas Vlachos <kvl_93@outlook.com>
2020-11-28 13:04:13 +0200

    Minor syntax changes.

--- src/BurstChat.Domain/Schema/Chat/Link.cs
+++ src/BurstChat.Domain/Schema/Chat/Link.cs
@@ -3,22 +3,22 @@ using System;
 namespace BurstChat.Domain.Schema.Chat
 {
     /// <summary>
-    ///     This class stores urls posted on messages.
+    /// This class stores urls posted on messages.
     /// </summary>
     public class Link
     {
         /// <summary>
-        ///     An identifier for the url.
+        /// An identifier for the url.
         /// </summary>
         public long Id { get; set; }
 
         /// <summary>
-        ///     The url posted.
+        /// The url posted.
         /// </summary>
         public string Url { get; set; } = string.Empty;
 
         /// <summary>
-        ///     The date the entry was created.
+        /// The date the entry was created.
         /// </summary>
         public DateTime DateCreated { get; set; }
     }
--- src/BurstChat.Domain/Schema/Servers/Invitation.cs
+++ src/BurstChat.Domain/Schema/Servers/Invitation.cs
@@ -5,52 +5,52 @@ using BurstChat.Domain.Schema.Users;
 namespace BurstChat.Domain.Schema.Servers
 {
     /// <summary>
-    ///     This class represents an assosiation between a server and an invitation sent to a user, for joining it.
+    /// This class represents an assosiation between a server and an invitation sent to a user, for joining it.
     /// </summary>
     public class Invitation
     {
         /// <summary>
-        ///     The identifier of the entry.
+        /// The identifier of the entry.
         /// </summary>
         public long Id { get; set; }
 
         /// <summary>
-        ///     The id of the server.
+        /// The id of the server.
         /// </summary>
         public int ServerId { get; set; }
 
         /// <summary>
-        ///   This is a reference navigation property of the server.
+        /// This is a reference navigation property of the server.
         /// </summary>
         public Server? Server { get; set; }
 
         /// <summary>
-        ///     The id of the target user.
+        /// The id of the target user.
         /// </summary>
         public long UserId { get;set; }
 
         /// <summary>
-        ///   This is a reference navigation property of the server.
+        /// This is a reference navigation property of the server.
         /// </summary>
         public User? User { get; set; }
 
         /// <summary>
-        ///     A flag specifying the invitation was accepted.
+        /// A flag specifying the invitation was accepted.
         /// </summary>
         public bool Accepted { get; set; }
 
         /// <summary>
-        ///     A flag specifying the invitation was declined.
+        /// A flag specifying the invitation was declined.
         /// </summary>
         public bool Declined { get; set; }
 
         /// <summary>
-        ///     The date that the entry updated either the Accepted or Declined properties.
+        /// The date that the entry updated either the Accepted or Declined properties.
         /// </summary>
         public DateTime? DateUpdated { get; set; }
 
         /// <summary>
-        ///     The date that the entry was created.
+        /// The date that the entry was created.
         /// </summary>
         public DateTime DateCreated { get; set; }
     }
